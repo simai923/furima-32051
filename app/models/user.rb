50940ecb421290ac21878d2f9ec225d04f737dc0
simai923 <ishun19497@gmail.com>
2020-11-26 20:46:53 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: {with: PASSWORD_REGEX, message:'半角英数字の両方を含めて設定してください' }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:'全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name 
+    end
+    with_options format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください'} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birth_date
+  end
+
 end
