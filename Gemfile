c0a4c4dbdb716b727a80517091daf8b3ea56a349
simai923 <ishun19497@gmail.com>
2020-11-27 21:41:32 +0900

    バリデーションの修正

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 4.0.0'
   gem 'factory_bot_rails'
   gem 'faker'
+  gem 'gimei'
 end
 
 group :development do
@@ -59,3 +60,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates :password, format: {with: PASSWORD_REGEX, message:'半角英数字の両方を含めて設定してください' }
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:'全角文字を使用してください' } do
+    validates :password, format: {with: PASSWORD_REGEX }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
       validates :last_name
       validates :first_name 
     end
-    with_options format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください'} do
+    with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
       validates :last_name_kana
       validates :first_name_kana
     end
