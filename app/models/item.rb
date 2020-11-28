8d0e9fe357d3494514dcd716a187a55393cce012
simai923 <ishun19497@gmail.com>
2020-11-28 16:40:48 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :ship_cost, :ship_from_area, :ship_day
+
+  validates :name, :description, :price, presence: true
+  validates :category_id, :condition_id, :ship_cost_id, :ship_from_area_id, :ship_day_id, numericality: { other_than: 1 }
+  belongs_to :user
 end
