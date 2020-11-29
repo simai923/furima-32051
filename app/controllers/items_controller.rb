7a947c774871bc5a1ccea072f5fd906a76c2f37a
simai923 <ishun19497@gmail.com>
2020-11-29 13:06:05 +0900

    createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :ship_cost_id, :ship_from_area_id, :ship_day_id, :price).merge(user_id: current_user.id)
   end
 end
