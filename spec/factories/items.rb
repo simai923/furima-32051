6badf6c6660b487b97ad8af960fed61c523d90e9
simai923 <ishun19497@gmail.com>
2020-11-30 16:36:48 +0900

    itemモデルのテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name              { 'テスト' }
+    description       {Faker::Lorem.sentence}
+    category_id       { 2 }
+    condition_id      { 3 }
+    ship_cost_id      { 4 }
+    ship_from_area_id { 5 }
+    ship_day_id       { 2 }
+    price             { 3456 }
+
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
