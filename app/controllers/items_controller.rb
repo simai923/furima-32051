8433e5f6b2f3d03ae69b7e022593a7b5c88d0b0a
simai923 <ishun19497@gmail.com>
2020-12-02 14:52:08 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+    
   end
 
   def show
