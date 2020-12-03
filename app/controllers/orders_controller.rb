7219d664a7af96cad27a657400f849b76c7c3154
simai923 <ishun19497@gmail.com>
2020-12-03 20:56:32 +0900

    購入画面の表示

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,7 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new
+  end
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :ship_cost, :ship_from_area, :ship_day
   belongs_to :user
+  has_one :order
   has_one_attached :image
+  
   with_options presence: true do
     validates :image, :name, :description
     validates :price, numericality: { only_integer: true, message: 'Half-width number' }
