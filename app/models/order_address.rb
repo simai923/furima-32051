f9627843982d826e08441a1ac4884467cd2cfff6
simai923 <ishun19497@gmail.com>
2020-12-08 14:12:27 +0900

    Formオブジェクト編集

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,16 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order
-
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :order_id
   with_options presence: true do
-    validates :postal_code, :city, :address, :phone_number, :order
+    validates :postal_code, :city, :address, :phone_number
   end
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
-    user = User.find(params[:id])
-    item = User.find(params[:id])
-    order = Order.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
